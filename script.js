b80dafda92d3190712a17b26888f9126ca2ea201
Patrick Sherrard <patsherrard@gmail.com>
2020-05-14 19:11:22 -0400

    want to check if mobile will work with jquery

--- script.js
+++ script.js
@@ -9,25 +9,35 @@ portfolio = {}
 //hover over this toggle projectOne get projectInfo1 shown
 
 //Maybe use time out feature for when css animation is done?
+//.slideDown()
+
+// ADD CSS THAT THAT APPLIES PADDING-BOTTOM
+//.css("propertyname","value");
+
+
+
 
 portfolio.projectHover = () => {
     $(".projectOneInfo").hide(); 
     $(".projectTwoInfo").hide();
     $(".projectThreeInfo").hide();
 
+    
     $(".projectOne").hover(function () {
-
-        $(".projectOneInfo").show();
+        $(".projectOneInfo").slideDown("fast");
         portfolio.mouseleaveOne();
     })
+    
+    // $(".projectOne").hover(function () {
+    //     $(".projectOneInfo").slideDown("fast");
+    //     portfolio.mouseleaveOne();
+    // })
     $(".projectTwo").hover(function () {
-
-        $(".projectTwoInfo").show();
+        $(".projectTwoInfo").slideDown("fast");
         portfolio.mouseleaveTwo();
     })
     $(".projectThree").hover(function () {
-
-        $(".projectThreeInfo").show();
+        $(".projectThreeInfo").slideDown("fast");
         portfolio.mouseleaveThree();
     })
 }
